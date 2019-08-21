192d991329875a95bef72dec89c143998e89eab4
Joshua Barker <jobarker@redventures.com>
2019-08-21 14:42:42 -0400

    added rgb, and hex conversions, and copy ability

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
+import Clipboard from 'clipboard';
+import { HSLToRGB, HSLToHex } from '../../utils/conversion';
 import './Header.css';
 
-function Header(props) {
-  const hue = props.hue;
-  const sat = `${props.saturation}%`;
-  const lts = `${props.lightness}%`;
-  return (
-    <header className="site-header">
-      <h2>iridescent</h2>
-      <button className="btn btn-hsl">{`hsl(${hue}, ${sat}, ${lts})`}</button>
-      <p>Find your color.</p>
-    </header>
-  )
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hue: this.props.hue,
+      sat: `${this.props.saturation}%`,
+      lts: `${this.props.lightness}%`,
+      hslVal: `hsl(${this.props.hue}, ${this.props.saturation}%, ${this.props.lightness}%)`,
+      rgbVal: HSLToRGB(this.props.hue, this.props.saturation, this.props.lightness),
+      hexVal: HSLToHex(this.props.hue, this.props.saturation, this.props.lightness)
+    }
+  }
+
+  componentDidMount() {
+    const cb = new Clipboard('.btn');
+    const success = document.querySelector('.success');
+    cb.on('success', (e) => {
+      success.classList.add('open');
+
+      setTimeout(() => {
+        success.classList.remove('open');
+      }, 2000)
+
+      e.clearSelection();
+    });
+  }
+
+  render() {
+    return (
+      <header className="site-header">
+        <h2>iridescent</h2>
+        <button className="btn btn-hsl" data-clipboard-text={this.state.hslVal}>{this.state.hslVal}</button>
+        <button className="btn btn-rgb" data-clipboard-text={this.state.rgbVal}>{this.state.rgbVal}</button>
+        <button className="btn btn-hex" data-clipboard-text={this.state.hexVal}>{this.state.hexVal}</button>
+        <p>Find your color.</p>
+        <div className="success">
+          <span>Copied!</span>
+        </div>
+      </header>
+    )
+  }
 }
 
 export default Header;
