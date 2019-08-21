04526ac02cfa6fe6b044cce920f48ca1ce404d72
Joshua Barker <jobarker@redventures.com>
2019-08-21 14:52:19 -0400

    Updates

---
+++ precache-manifest.cff8ac22ba5fd9619c12a8c5b57df13f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c80e6495638888a5fa1d20717c5e83a",
+    "url": "/iridescent/index.html"
+  },
+  {
+    "revision": "238a75f9b9b68e88b243",
+    "url": "/iridescent/static/css/main.0b14d8ee.chunk.css"
+  },
+  {
+    "revision": "84909eab481541e5580c",
+    "url": "/iridescent/static/js/2.84e5c014.chunk.js"
+  },
+  {
+    "revision": "238a75f9b9b68e88b243",
+    "url": "/iridescent/static/js/main.072f2a40.chunk.js"
+  },
+  {
+    "revision": "26eb2d86d6323d19e385",
+    "url": "/iridescent/static/js/runtime~main.3650ba7f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3caabd4a22e600419d1d743cf809bc1c.js"
+  "/iridescent/precache-manifest.cff8ac22ba5fd9619c12a8c5b57df13f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iridescent/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
